ef836b8606f327a6555a413061bf44c0358f515f
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-25 00:33:55 +0900

    font変更テスト

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,8 +1,22 @@
-import styles from '../../ui/home.module.css'
+import styles from './customers.module.css'
 
 export default function Page() {
   return <main>
     <p>Customers Page</p>
-    <div className={styles.shape}>aaaaaaa</div>
+    <div className={styles.button}>aaaaaaa</div>
+    <div className={styles.button}>bbbbbbb</div>
   </main>;
+}
+
+type t = {
+  foo: string;
+  bar: number;
+  buzz: boolean;
+}
+
+export function f(): any {
+  const obj1: t = { foo: 'foo1', bar: 42, buzz: true };
+  const { foo, bar } = obj1;
+  console.log(foo)
+  console.log(obj1.foo)
 }
