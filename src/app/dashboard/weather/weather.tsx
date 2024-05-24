a38e8c5a1f6e1319cf24667d13e266b7de0b99b5
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-25 00:34:13 +0900

    weatherApi作成

---
+++ src/app/dashboard/weather/weather.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+type WeatherDataResponse = {
+  weatherId: string;
+  label: string;
+  title: string;
+};
+
+export async function getWeatherResponse(): Promise<WeatherDataResponse | null> {
+  try {
+    const response = await axios.get(`http://localhost:34962/weather`);
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+}
