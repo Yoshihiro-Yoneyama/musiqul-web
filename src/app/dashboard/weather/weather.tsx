f76a8b00cd403141a73fc3f581d87ca070b7733e
yoshihiro <yoshihiro>
2024-07-22 16:47:15 +0900

    フロントとのAPI通信テスト

--- src/app/dashboard/weather/weather.tsx
+++ src/app/dashboard/weather/weather.tsx
@@ -9,6 +9,28 @@ type WeatherDataResponse = {
 export async function getWeatherResponse(): Promise<WeatherDataResponse | null> {
   try {
     const response = await axios.get(`http://localhost:34962/api/weather`);
+    const response2 = await axios.post(
+        `http://localhost:34962/recruitment`,
+        `{
+        "name": "フロントからtest2",
+        "owner": "d39c9cdb-759c-479e-94c7-2ddc0a3044e8",
+        "genre": ["ROCK", "ANIME"],
+        "songTitle": "title",
+        "artist": "artist",
+        "ownerInstruments": ["VOCAL", "ELECTRIC_BASE"],
+        "recruitedInstruments": {"VOCAL": 1},
+        "requiredGenerations": ["TEEN", "TWENTIES"],
+        "requiredGender": "MALE_ONLY",
+        "deadline": "2024-07-08T00:00:00+09:00",
+        "memo": "memo"
+      }`,
+        {
+          headers: {
+            "content-type": 'application/json',
+            }
+        }
+      )
+    ;
     return response.data;
   } catch (error) {
     return null;
