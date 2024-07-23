529cf15a1f64f15e4560e5040f249a02575964cd
yoshihiro <yoshihiro>
2024-07-23 19:42:16 +0900

    cssの設定

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import space from "@/themes/space";
+import fontSizes from "@/themes/fontSizes";
+import letterSpacings from "@/themes/letterSpacings";
+import lineHeights from "@/themes/lineHeights";
+import colors from "@/themes/colors";
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const
