cd0ef193d8ada7545c078c85ba3124ba74466340
yoshihiro <yoshihiro>
2024-09-28 18:34:20 +0900

    AtomでButton作成

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,37 @@
 "use client"
 
+import styled from "styled-components";
 import React from "react";
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger'
+type ButtonProps = {
+  color: string;
+  background: string;
+  label: string;
+  onClick: () => void;
+};
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+const StyledButton = styled.button<ButtonProps>`
+    color: ${(props) => props.color};
+    background: ${(props) => props.background};
+    border: 2px solid ${(props) => props.color};
+
+    font-size: 1.5em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border-radius: 8px;
+    cursor: pointer;
+
+    &:hover {
+        font-size: 1.6em;
+    }
+`;
+
+const Button: React.FC<ButtonProps> = ({ color, background, label, onClick }) => {
+  return (
+    <StyledButton color={color} background={background} onClick={onClick} label={label}>
+      {label}
+    </StyledButton>
+  );
+};
+
+export default Button;
