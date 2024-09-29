7245cb02e6e3e4ac8aabe0b822508fdac40019d0
yoshihiro <yoshihiro>
2024-09-29 22:29:57 +0900

    値処理クラス

--- src/app/collab/recruitment/component/createRecruitment.tsx
+++ src/app/collab/recruitment/component/createRecruitment.tsx
@@ -1,11 +1,21 @@
+import axios from "axios";
+
 type Recruitment = {
   name: string,
   owner: string,
-  genres: string[],
-  songTitle: string,
-  
+  // genres: string[],
+  // songTitle: string,
+  // artist: string,
+  // ownerInstruments: string[],
+  // recruitedInstruments: string[],
+  // requiredGenerations: string[],
+  // requiredGender: string,
+  // deadline: Date
 }
 
 const createRecruitment = () => {
-
+  axios.post(
+    `/recruitment`,
+    
+  )
 }
