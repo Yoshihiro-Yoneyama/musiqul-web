dd5326622d1a6c02a3377bd2c73a995c6b4e9be3
yoshihiro <yoshihiro>
2024-10-04 22:48:32 +0900

    HomeとLayoutのCSS作成

---
+++ src/app/styles.css.ts
@@ -0,0 +1,28 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  padding: 20,
+  backgroundColor: "pink",
+  fontSize: 50,
+  color: "#fff",
+});
+
+export const BodyContainer = style({
+  backgroundColor: "#000000"
+})
+
+export const FlexContainer = style({
+  display: "flex",
+  height: "100vh"
+});
+
+export const SideMenuContainer = style({
+  width: "250px",
+  padding: "20px"
+})
+
+export const ChildrenContainer = style({
+  flex: "1",
+  color: "#ffffff",
+  padding: "100px"
+})
