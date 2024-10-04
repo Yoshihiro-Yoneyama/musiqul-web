cecb4954d5909c72d4816edfb73e562576619f7c
yoshihiro <yoshihiro>
2024-10-04 22:46:53 +0900

    Home画面を仮作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,22 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
+import * as styles from "./styles.css";
 import {useRouter} from "next/navigation";
-import Button from "@/components/atoms/Button";
-
-const HomePageStyle = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #000000;
-    font-family: Arial, sans-serif;
-    color: #ffffff;
-
-    h1 {
-        font-size: 2.5rem;
-    }
-`;
-
 
 const HomePage = () => {
-
+  
   const useRouter1 = useRouter()
-
+  
   const onSubmit = () => {
     useRouter1.push('/collab/recruitment')
   }
-
+  
   return (
-    <HomePageStyle>
-      <h1>Welcome to My Web App!</h1>
-      <Button
-        color="white"
-        background="blue"
-        label="Go to Page"
-        onClick={onSubmit}
-      />
-    </HomePageStyle>
+    <main className={styles.container}>
+      <div className={styles.container}>Hello World🙌</div>
+      <button onClick={onSubmit}/>
+    </main>
   );
 };
 
