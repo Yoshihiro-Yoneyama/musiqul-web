9d783ab11720595eb85fb222eada1416ec12c22b
yoshihiro <yoshihiro>
2024-10-05 16:12:08 +0900

    ホームレイアウトを作成する

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,22 @@
 "use client"
 
 import React from "react";
-import styled from "styled-components";
 import SideMenu from "@/app/sideMenu";
-
-const BodyLayout = styled.body`
-  background-color: #000000;
-`
-
-// サイドメニューを
-const FlexContainer = styled.div`
-  display: flex;
-  height: 100vh
-`;
-
-const SideMenuLayout = styled.div`
-  width: 250px;
-  padding: 20px;
-`;
-
-const ChildrenLayout = styled.div`
-  flex: 1;
-  color: #ffffff;
-  padding: 100px;
-`;
+import * as styles from "./styles.css"
 
 export default function RootLayout({
-                                     children,
-                                   }: {
+   children,
+ }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <BodyLayout>
-        <FlexContainer>
-          <SideMenuLayout>
-            <SideMenu/>
-          </SideMenuLayout>
-          <ChildrenLayout>
-            {children}
-          </ChildrenLayout>
-          
-        </FlexContainer>
-      </BodyLayout>
+      <body className={styles.BodyContainer}>
+        <div className={styles.FlexContainer}>
+          <div className={styles.SideMenuContainer}><SideMenu/></div>
+          <div className={styles.ChildrenContainer}>{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
