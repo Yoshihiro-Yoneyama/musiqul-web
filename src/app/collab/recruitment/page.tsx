cb36139bfd5032941d8c0c6470fe902b6c058ef0
yoshihiro <yoshihiro>
2024-10-05 16:12:28 +0900

    コラボページを仮作成

--- src/app/collab/recruitment/page.tsx
+++ src/app/collab/recruitment/page.tsx
@@ -1,115 +1,70 @@
 "use client"
 
-import React, {FormEvent} from "react";
-import {useRouter} from "next/navigation";
-import Button from "@/components/atoms/Button";
-import axios from "axios";
-import api from "@/utils/axios/axios.config";
-
-type Recruitment = {
-  name: string,
-  // owner: string,
-  // genres: string[],
-  songTitle: string,
-  // artist: string,
-  // ownerInstruments: string[],
-  // recruitedInstruments: string[],
-  // requiredGenerations: string[],
-  // requiredGender: string,
-  // deadline: Date
-}
+import React from "react";
+import {createRecruitment} from "@/app/collab/recruitment/component/createRecruitment";
+import Button from "@/components/atoms/button/Button";
 
 const Recruitment = () => {
-
-  const useRouter1 = useRouter()
-  const onSubmit = () => {
-    // useRouter1.push('/')
-  }
-  
-
-  
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    
-    // フォームデータの取得
-    const formData = new FormData(event.currentTarget);
-    
-    // Recruitment型に翻訳
-    const recruitmentData: Recruitment = {
-      name: formData.get("name") as string,  // 必須フィールドなので型アサーション
-      // owner: formData.get("owner") as string, // 必須フィールドなので型アサーション
-      // genres: (formData.get("genres") as string).split(","), // 配列に変換
-      songTitle: formData.get("songTitle") as string,
-      // artist: formData.get("artist") as string,
-      // ownerInstruments: (formData.get("ownerInstruments") as string).split(","), // カンマ区切りで配列に
-      // recruitedInstruments: (formData.get("recruitedInstruments") as string).split(","),
-      // requiredGenerations: (formData.get("requiredGenerations") as string).split(","),
-      // requiredGender: formData.get("requiredGender") as string,
-      // deadline: new Date(formData.get("deadline") as string) // Date型に変換
-    };
-    
-    // APIリクエスト送信
-    event.preventDefault();
-    try {
-      await api.post('/recruitment', recruitmentData);
-      console.log("Recruitment data sent successfully");
-    } catch (error) {
-      console.error("Error sending recruitment data:", error);
-    }
-    
-    
-    // event.preventDefault();
-    // const data = new FormData(event.currentTarget)
-    // await api.post('/recruitment', {
-    //   name: data.get("name")
-    // })
-  }
-
+  const onSubmit = () => {}
   return (
     <h1>コラボを登録する
-      <form onSubmit={event => handleSubmit(event)}>
-        <div>
+      <form onSubmit={event => createRecruitment(event)}>
+        <label>
+          コラボ名: <input id="name" name="name"/>
+        </label>
+        <hr/>
+        {/*チェックボックスの選択肢を自動で表示したい*/}
+        <label>
+          ジャンル:
           <label>
-            コラボ名: <input id="name" name="name"/>
+            <input
+              type="checkbox"
+              name="genre"
+              value="Rock"
+            />
+            Rock
           </label>
-          <hr/>
-          {/*<label>*/}
-          {/*  ジャンル: <input name="genre"/>*/}
-          {/*</label>*/}
-          {/*<hr/>*/}
           <label>
-            曲名: <input name="songTitle"/>
+            <input
+              type="checkbox"
+              name="genre"
+              value="Jazz"
+            />
+            Jazz
           </label>
-          {/*<hr/>*/}
-          {/*<label>*/}
-          {/*  アーティスト名: <input name="artist"/>*/}
-          {/*</label>*/}
-          {/*<hr/>*/}
-          {/*<label>*/}
-          {/*  コラボ元の楽器: <input name="ownerInstruments"/>*/}
-          {/*</label>*/}
-          {/*<hr/>*/}
-          {/*<label>*/}
-          {/*  募集楽器: <input name="recruitedInstruments"/>*/}
-          {/*</label>*/}
-          {/*<label>*/}
-          {/*  人数: <input name="countOfInstruments"/>*/}
-          {/*</label>*/}
-          <hr/>
-          {/*<input type="submit" defaultValue={"Submit"}/>*/}
-          <Button
-            color="white"
-            background="blue"
-            label="登録する"
-            onClick={onSubmit}
-          />
-        </div>
-        
-        {/*<Button*/}
-        {/*  color="white"*/}
-        {/*  background="blue"*/}
-        {/*  label="Go to Page"*/}
-        {/*  onClick={onSubmit}*/}
-        {/*/>*/}
+          <label>
+            <input
+              type="checkbox"
+              name="genre"
+              value="Pop"
+            />
+            Pop
+          </label>
+        </label>
+        <hr/>
+        <label>
+          曲名: <input name="songTitle"/>
+        </label>
+        <hr/>
+          <label>
+            アーティスト名: <input name="artist"/>
+          </label>
+        <hr/>
+        {/*<label>*/}
+        {/*  コラボ元の楽器: <input name="ownerInstruments"/>*/}
+        {/*</label>*/}
+        {/*<hr/>*/}
+        {/*<label>*/}
+        {/*  募集楽器: <input name="recruitedInstruments"/>*/}
+        {/*</label>*/}
+        {/*<label>*/}
+        {/*  人数: <input name="countOfInstruments"/>*/}
+        {/*</label>*/}
+        {/*<hr/>*/}
+        {/*<input type="submit" defaultValue={"登録する"}/>*/}
+        <Button variant={"danger"} onClick={onSubmit}>
+          登録する
+        </Button>
       </form>
     </h1>
   );
