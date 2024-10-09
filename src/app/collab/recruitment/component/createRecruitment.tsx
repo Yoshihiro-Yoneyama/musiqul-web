355463638dbe7af9c30b8310d5a073b524313eff
yoshihiro <yoshihiro>
2024-10-09 22:47:10 +0900

    Eitherを実装

--- src/app/collab/recruitment/component/createRecruitment.tsx
+++ src/app/collab/recruitment/component/createRecruitment.tsx
@@ -3,10 +3,10 @@ import api from "@/utils/axios/axios.config";
 
 type RecruitmentRequest = {
   name: string,
-  // owner: string,
+  owner: string,
   genres: string[],
   songTitle: string,
-  // artist: string,
+  artist: string,
   // ownerInstruments: string[],
   // recruitedInstruments: string[],
   // requiredGenerations: string[],
@@ -19,17 +19,16 @@ export const createRecruitment = async (event: FormEvent<HTMLFormElement>) => {
   // フォームデータの取得
   const formData = new FormData(event.currentTarget);
   
-  
   // チェックボックスの値を取得（`genre` の配列を正しく取得するため）
   const genres: string[] = formData.getAll("genre").map(String);
   
   // Recruitment型に翻訳
   const recruitmentData: RecruitmentRequest = {
     name: formData.get("name") as string,  // 必須フィールドなので型アサーション
-    // owner: formData.get("owner") as string, // 必須フィールドなので型アサーション
+    owner: formData.get("owner") as string, // 必須フィールドなので型アサーション
     genres: genres,
     songTitle: formData.get("songTitle") as string,
-    // artist: formData.get("artist") as string,
+    artist: formData.get("artist") as string,
     // ownerInstruments: (formData.get("ownerInstruments") as string).split(","), // カンマ区切りで配列に
     // recruitedInstruments: (formData.get("recruitedInstruments") as string).split(","),
     // requiredGenerations: (formData.get("requiredGenerations") as string).split(","),
