c19dea7606a60dfaa35cb90f98235238c908ca03
yoshihiro <yoshihiro>
2024-10-13 15:47:40 +0900

    Create Requirement Component

---
+++ src/components/atoms/requirement/Requirement.tsx
@@ -0,0 +1,9 @@
+import * as styles from "./requirement.css"
+
+const Requirement = () => {
+  return (
+    <div className={styles.Label}>必須</div>
+  )
+}
+
+export default Requirement
---
+++ src/components/atoms/requirement/requirement.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const Label = style({
+  width: "32px",
+  height: "20px",
+  backgroundColor: "#BF2A37",
+  color: "#FFFFFF",
+  fontSize: "12px"
+})
