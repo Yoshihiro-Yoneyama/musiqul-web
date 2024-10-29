4381670a1cfa9cb52dd3810ae72569c939143ee1
yoshihiro <yoneyonekun1@gmail.com>
2024-10-29 22:43:28 +0900

    Create DateCalendar component.

---
+++ src/components/util/Input.css.ts
@@ -0,0 +1,28 @@
+import {style} from '@vanilla-extract/css';
+
+export const inputStyles = {
+  default: style({
+    padding: "0",
+    fontSize: "16px",
+    borderRadius: "4px",
+    border: "none",
+    width: "280px",
+    height: "40px",
+    backgroundColor: "#1F1F1F",
+  }),
+  hover: style({
+    backgroundColor: "rgba(31, 31, 31, 0.7)",
+  }),
+  focused: style({
+    backgroundColor: "rgba(31, 31, 31, 0.7)",
+    border: "1px solid #FFFFFF",
+    color: "#FFFFFF",
+  }),
+  active: style({
+    color: "#FFFFFF",
+  }),
+  disabled: style({
+    backgroundColor: "rgba(31, 31, 31, 0.4)",
+    cursor: "not-allowed",
+  })
+}
