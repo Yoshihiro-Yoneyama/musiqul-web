3d5de36cbaf66d646fbfd77fd515522670c157af
yoshihiro <yoneyonekun1@gmail.com>
2024-11-08 22:37:32 +0900

    Make requiredNumberOfInstruments for SelectorComponent used by collab page.

--- src/app/collab/recruitment/page.tsx
+++ src/app/collab/recruitment/page.tsx
@@ -24,6 +24,20 @@ const genres: SelectorOption[] = [
   {value: 'POP', label: 'ポップ'},
 ];
 
+const requiredNumberOfInstruments: SelectorOption[] = [
+  {value: '0', label: '0'},
+  {value: '1', label: '1'},
+  {value: '2', label: '2'},
+  {value: '3', label: '3'},
+  {value: '4', label: '4'},
+  {value: '5', label: '5'},
+  {value: '6', label: '6'},
+  {value: '7', label: '7'},
+  {value: '8', label: '8'},
+  {value: '9', label: '9'},
+  {value: '10', label: '10'},
+]
+
 const recruitedInstruments: SelectorOption[] = [
   {value: '', label: ''},
   {value: 'VOCAL', label: 'ボーカル'},
@@ -133,19 +147,7 @@ const Recruitment = () => {
               id={"recruitedInstruments"}
               name={"recruitedInstruments"}
               options1={recruitedInstruments.map(value => value)}
-              options2={[
-                {value: '0', label: '0'},
-                {value: '1', label: '1'},
-                {value: '2', label: '2'},
-                {value: '3', label: '3'},
-                {value: '4', label: '4'},
-                {value: '5', label: '5'},
-                {value: '6', label: '6'},
-                {value: '7', label: '7'},
-                {value: '8', label: '8'},
-                {value: '9', label: '9'},
-                {value: '10', label: '10'},
-              ]}
+              options2={requiredNumberOfInstruments.map(value => value)}
               disabled={false}
             />
           </div>
