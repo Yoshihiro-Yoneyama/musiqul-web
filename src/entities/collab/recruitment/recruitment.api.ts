26ac89dbcda4facf6c1527d924c323dacf2df332
yoshihiro <yoneyonekun1@gmail.com>
2024-11-11 23:25:49 +0900

    set recruitment.api.ts

---
+++ src/entities/collab/recruitment/recruitment.api.ts
@@ -0,0 +1,7 @@
+import {RecruitmentSchema} from "@/entities/collab/recruitment/recruitment.model";
+import {AxiosInstance} from "axios";
+
+export const recruitment = async (
+  recruitment: RecruitmentSchema,
+  client: AxiosInstance
+) => {}
