ef447f087723e0a681422dd651df1e0d02ec8b4a
yoshihiro <yoneyonekun1@gmail.com>
2024-11-13 23:59:47 +0900

    Add SongTitle Field.

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// axios設定
+
 const api = axios.create()
 api.defaults.baseURL = 'http://localhost:34962';
 api.defaults.headers.post['Content-Type'] = 'application/json';
