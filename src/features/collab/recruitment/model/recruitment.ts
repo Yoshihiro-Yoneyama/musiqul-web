5dc60fbdce846eb6181166f79d597a402771e74d
yoshihiro <yoneyonekun1@gmail.com>
2024-11-15 22:30:53 +0900

    Add API Docs to recruitmentState.ts

---
+++ src/features/collab/recruitment/model/recruitment.ts
@@ -0,0 +1,34 @@
+import {RecruitmentSchema} from "@/entities/collab/recruitment/recruitment.model";
+import {postRecruitment} from "@/entities/collab/recruitment/recruitment.api";
+
+/**
+ * Creating a New Recruitment Entry
+ *
+ * @param recruitment Recruitment Status Component
+ */
+export const recruitment = async (
+  recruitment: RecruitmentSchema
+) => {
+  const newRecruit: RecruitmentSchema = {
+    owner: recruitment.owner,
+    // ownerInstruments: string[],
+    songTitle: recruitment.songTitle,
+    artist: recruitment.artist,
+    name: recruitment.name,
+    genre: recruitment.genre,
+    deadline: recruitment.deadline,
+    requiredGenerations: recruitment.requiredGenerations,
+    requiredGender: recruitment.requiredGender,
+    // map形式
+    // recruitedInstruments: string[],
+  }
+  
+  const result = await (postRecruitment(newRecruit))
+
+  // TODO　エラーの処理記述
+  
+  return {
+    ...recruitment,
+    // errors: undefined,
+  }
+}
