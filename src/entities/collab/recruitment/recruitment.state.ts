aa73df3c67deef8029a45789448744c9872ace7c
yoshihiro <yoneyonekun1@gmail.com>
2024-11-17 20:35:02 +0900

    fix(recruitment.state.ts): Fix indent.

--- src/entities/collab/recruitment/recruitment.state.ts
+++ src/entities/collab/recruitment/recruitment.state.ts
@@ -4,19 +4,19 @@ import {RecruitmentSchema} from "@/entities/collab/recruitment/recruitment.model
 import {atom, useAtom } from "jotai"
 import {useCallback} from "react"
 
+// Define the initial values for recruitment
+const recruitmentAtom = atom<RecruitmentSchema>({
+  owner: '',
+  name: '',
+  genre: [],
+  deadline: '',
+  requiredGenerations: [],
+  requiredGender: '',
+});
+
 export const useRecruitment = () => {
-  
-  // Define the initial values for recruitment
-  const recruitmentAtom = atom<RecruitmentSchema>({
-    owner: '',
-    name: '',
-    genre: [],
-    deadline: '',
-    requiredGenerations: [],
-    requiredGender: '',
-  });
-  
   const [recruitment, setRecruitment] = useAtom(recruitmentAtom)
+  
   const updateRecruitment = useCallback(
     (updatedRecruitment: RecruitmentSchema) => {
       setRecruitment(() => {
