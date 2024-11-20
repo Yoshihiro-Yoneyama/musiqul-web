ba74909721174aaa2edf70a252a978c12553e102
yoshihiro <yoneyonekun1@gmail.com>
2024-11-20 22:22:54 +0900

    feat( recruitment.state.ts ): Add owner placeholder.

--- src/entities/collab/recruitment/recruitment.state.ts
+++ src/entities/collab/recruitment/recruitment.state.ts
@@ -8,7 +8,8 @@ import {useCallback} from "react"
  * Define the initial values for recruitment
  */
 const recruitmentAtom = atom<RecruitmentSchema>({
-  owner: '',
+  // Placeholder for the owner
+  owner: 'd39c9cdb-759c-479e-94c7-2ddc0a3044e8',
   name: '',
   genre: [],
   deadline: '',
