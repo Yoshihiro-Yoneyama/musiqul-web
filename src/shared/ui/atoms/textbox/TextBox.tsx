15a8d49096f1ae3e0119677d02eb3a7fc8d9306a
yoshihiro <yoneyonekun1@gmail.com>
2024-12-08 22:07:35 +0900

    Refactor ( TextBox.tsx ): Add props.

--- src/shared/ui/atoms/textbox/TextBox.tsx
+++ src/shared/ui/atoms/textbox/TextBox.tsx
@@ -1,13 +1,22 @@
 "use client"
 
-import React, {ChangeEvent, useState} from "react"
+import React, {ChangeEvent, ReactNode, useState} from "react"
 import {Input as AriaInput} from "react-aria-components"
 import {inputStyles} from "@/shared/ui/util/Input.css"
 import {clsx} from "clsx";
 import namedMemo from "@/shared/hooks/namedMemo";
+import fontSizes from "@/shared/themes/fontSizes";
+import {fontWeights} from "@/shared/themes/fontWeights";
+import {colors} from "@/shared/themes/colors";
+import {lineHeights} from "@/shared/themes/lineHeights";
 
 export type TextBoxProps = {
-  readonly name?: string
+  readonly children?: ReactNode
+  readonly size?: keyof typeof fontSizes
+  readonly weight?: keyof typeof fontWeights
+  readonly color?: keyof typeof colors
+  readonly lineHeight?: keyof typeof lineHeights
+  readonly as?: string
   readonly type?: string
   readonly inputValue?: string
   readonly valueType?: string | number | undefined
@@ -15,12 +24,19 @@ export type TextBoxProps = {
   // readonly errorMessages?: string
   // readonly isInvalid?: boolean
   readonly isDisabled?: boolean
-  readonly onChange: (value: string) => void
+  readonly onChange?: (value: string) => void
   readonly autoComplete?: string
   // readonly maxLength?: number
+  readonly testId?: string
 }
 
 const TextBox: React.FC<TextBoxProps> = ({
+  children = '',
+  size = 'l',
+  weight = 'regular',
+  color = 'bodyText',
+  lineHeight = 'l',
+  as = 'p',
   isDisabled,
   onChange,
   autoComplete,
