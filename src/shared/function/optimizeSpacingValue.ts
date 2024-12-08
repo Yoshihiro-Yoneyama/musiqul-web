f6d500515c76b60166f922f1105becd23b55e8a6
yoshihiro <yoneyonekun1@gmail.com>
2024-12-08 19:56:42 +0900

    Feat ( optimizeSpacingValue.ts ): Create optimizeSpacingValue.ts.

---
+++ src/shared/function/optimizeSpacingValue.ts
@@ -0,0 +1,34 @@
+import {Space} from "@/shared/themes/spaces";
+
+type SpacingValues = '0' | Space
+type SpacingValueType = {
+  all?: SpacingValues
+  x?: SpacingValues
+  y?: SpacingValues
+  top?: SpacingValues
+  right?: SpacingValues
+  bottom?: SpacingValues
+  left?: SpacingValues
+}
+
+const optimizeSpacingValue = ({
+  all,
+  x,
+  y,
+  top,
+  right,
+  bottom,
+  left
+}: SpacingValueType): Record<
+  'top' | 'right' | 'bottom' | 'left',
+  SpacingValues
+> => {
+  return {
+    top: top ?? y ?? all ?? '0',
+    right: right ?? x ?? all ?? '0',
+    bottom: bottom ?? y ?? all ?? '0',
+    left: left ?? x ?? all ?? '0'
+  }
+}
+
+export default optimizeSpacingValue
