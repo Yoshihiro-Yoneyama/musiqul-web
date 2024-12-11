d6e0ce86319870b0819fc06fdf83010c6983d108
yoshihiro <yoneyonekun1@gmail.com>
2024-12-11 23:57:27 +0900

    Refactor ( Textbox.css.ts ): Use shared css.

--- src/shared/ui/atoms/textbox/Textbox.css.ts
+++ src/shared/ui/atoms/textbox/Textbox.css.ts
@@ -1,8 +1,10 @@
 import {style} from '@vanilla-extract/css';
+import color from "@/shared/themes/colors"
+import {fontSizes} from "@/shared/themes/fontSizes";
 
 export const inputStyles = {
   default: style({
-    fontSize: "16px",
+    fontSize: fontSizes.m,
     borderRadius: "4px",
     border: "none",
     width: "280px",
@@ -15,10 +17,10 @@ export const inputStyles = {
   focused: style({
     backgroundColor: "rgba(31, 31, 31, 0.7)",
     border: "1px solid #FFFFFF",
-    color: "#FFFFFF",
+    color: color.white,
   }),
   active: style({
-    color: "#FFFFFF",
+    color: color.white,
   }),
   disabled: style({
     backgroundColor: "rgba(31, 31, 31, 0.4)",
