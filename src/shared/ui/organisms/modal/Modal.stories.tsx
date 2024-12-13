8fa6b4b443b346bf7079a9cc75e5a832278887a3
yoshihiro <yoneyonekun1@gmail.com>
2024-12-13 23:57:38 +0900

    Feat ( Modal.stories.tsx ) Create Modal.stories.tsx:

---
+++ src/shared/ui/organisms/modal/Modal.stories.tsx
@@ -0,0 +1,21 @@
+import {Meta, StoryObj} from "@storybook/react";
+import Modal from "@/shared/ui/organisms/modal/Modal";
+
+const meta : Meta<typeof Modal> = {
+  title: 'organisms/modal/Modal',
+  component: Modal,
+  tags: ['autodocs', 'component']
+} satisfies Meta<typeof Modal>
+
+export default meta
+
+type Story = StoryObj<typeof Modal>
+
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    dialogAriaLabel: 'テスト',
+    children: 'テスト',
+    
+  }
+}
