a6beeab9bf5c1ef5e655a1df02b8738da3de5c66
yoshihiro <yoneyonekun1@gmail.com>
2024-12-15 10:54:42 +0900

    Feat ( useModal.ts ) Create useModal.ts.

---
+++ src/shared/hooks/useModal.ts
@@ -0,0 +1,40 @@
+import {ModalProps} from "@/shared/ui/organisms/modal/Modal";
+import {atom, useAtom} from "jotai";
+import {useCallback, useMemo} from "react";
+
+type ModalOptions = Omit<ModalProps, 'isOpen' | 'onOpenChange'>
+
+const isOpenAtom = atom(false)
+
+const modalOptionsAtom = atom<ModalOptions>({
+  children: null,
+  dialogAriaLabel: 'モーダル',
+  isXClose: false,
+  isDismissable: true,
+})
+
+const useModal = () => {
+  const [isOpen, setIsOpen] = useAtom(isOpenAtom)
+  const [modalOptions, setModalOptions] = useAtom(modalOptionsAtom)
+  
+  const openModal = useCallback(
+    (options: ModalOptions) => {
+      setModalOptions(options)
+      setIsOpen(true)
+    },
+    [setModalOptions, setIsOpen]
+  )
+  
+  const closeModal = useCallback(() => {
+    setIsOpen(false)
+  }, [setIsOpen])
+  
+  return useMemo(() => ({
+      isOpen,
+      modalOptions,
+      openModal,
+      closeModal,
+    }),
+    [isOpen, modalOptions, openModal, closeModal]
+  )
+}
