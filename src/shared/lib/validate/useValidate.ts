861b64c68d6cbf85a0135eef9663b0d3b056774c
yoshihiro <yoneyonekun1@gmail.com>
2025-02-23 12:57:11 +0900

    Feat ( useValidate.ts ) implement useValidate.useValidation

--- src/shared/lib/validate/useValidate.ts
+++ src/shared/lib/validate/useValidate.ts
@@ -1,1 +1,27 @@
-import { useState, useEffect } from 'react';
+import {ZodSchema, ZodError, infer as ZodInfer} from "zod";
+import {useCallback, useState} from "react";
+
+type ErrorMessages<T> = Partial<Record<keyof T, string>>
+
+export const useValidation = <T extends ZodSchema>() => {
+  const [errorMessages, setErrorMessages] = useState<ErrorMessages<ZodInfer<T>>>({})
+  
+  const validate = useCallback((values: ZodInfer<T>, schema: T) => {
+    const errors: ErrorMessages<Zod.infer<T>> = {}
+    
+    try {
+      schema.parse(values)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        error.errors.forEach((e) => {
+          const key = e.path.join('.') as keyof ZodInfer<T>
+        })
+      }
+    }
+    
+    setErrorMessages(errors)
+    return Object.keys(errors).length > 0
+  }, [])
+  
+  return { errorMessages, validate, setErrorMessages }
+}
