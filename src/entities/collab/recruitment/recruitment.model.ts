d01a3ed5e585f22e971db94349af43897fa0e0fd
yoshihiro <yoneyonekun1@gmail.com>
2025-03-08 14:31:24 +0900

    Feat ( recruitment.model.ts ) Implement validation in recruitmentSchema

--- src/entities/collab/recruitment/recruitment.model.ts
+++ src/entities/collab/recruitment/recruitment.model.ts
@@ -1,17 +1,29 @@
 import {z} from "zod";
+import customErrorMap from "@/shared/lib/validate/customErrorMap";
 
 export const recruitmentSchema = z.object({
   owner: z.string(),
   ownerInstruments: z.string().array(),
-  songTitle: z.string().optional(),
-  artist: z.string().optional(),
-  name: z.string().min(1),
+  songTitle: z.string({ errorMap: customErrorMap })
+    .min(1)
+    .max(500)
+    .optional(),
+  artist: z.string({ errorMap: customErrorMap })
+    .min(1)
+    .max(100)
+    .optional(),
+  name: z.string({ errorMap: customErrorMap })
+    .min(1)
+    .max(100),
   genres: z.string().array(),
   deadline: z.string(),
   requiredGenerations: z.string().array(),
   requiredGenders: z.string().array(),
   recruitedInstruments: z.map(z.string(), z.number()),
-  memo: z.string().optional(),
+  memo: z.string({ errorMap: customErrorMap })
+    .min(1)
+    .max(2000)
+    .optional(),
 })
 
 export type RecruitmentSchema = z.infer<typeof recruitmentSchema>
